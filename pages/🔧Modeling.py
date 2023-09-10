23a517ed0d1a0a8488d88ce8e6b6674a8ef0275b
Varun Chindage <98298554+Va-un@users.noreply.github.com>
2023-09-10 14:52:39 +0530

    Update 🔧Modeling.py

--- pages/🔧Modeling.py
+++ pages/🔧Modeling.py
@@ -68,13 +68,13 @@ Region = st.selectbox("",['southwest', 'southeast', 'northwest', 'northeast'])
 
 Predict = st.button("Predict")
 
-bmi = NUM.transform([[BMI]]).tolist()
-Value = [Gen[Gender], smoker[Smoker], region[Region], Age, 0.34, Child ]
+bmi = NUM.transform([[BMI]])
+Value = [Gen[Gender], smoker[Smoker], region[Region], Age, bmi, Child ]
 
 if (Predict):
     with st.spinner('Estimating The Cost'):
         time.sleep(0)
     #Cost = f'The Estimated money required is  ${predection(Value)}'
-    st.subheader(predection(Value))
+    st.subheader(bmi)
 
 
